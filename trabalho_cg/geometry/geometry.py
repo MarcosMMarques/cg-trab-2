7a5bb5f4f7bfcc5e498961f2f981fedcaa7c9a0e
felkng <felipersf374@gmail.com>
2024-08-10 16:09:45 -0300

    Add Geometry superclass
    What was added:
    - _id_counter to differentiate each figure
    - toString(), getFigure() and getId() methods

--- trabalho_cg/geometry/geometry.py
+++ trabalho_cg/geometry/geometry.py
@@ -1,34 +1,58 @@
 from typing import Tuple
 
 
-class Point():
-    def __init__(self, point: Tuple):
+class Geometry:
+    _id_counter = 0
+
+    def __init__(self):
+        self.id = Geometry._generate_id()
+
+    @classmethod
+    def _generate_id(cls) -> int:
+        cls._id_counter += 1
+        return cls._id_counter
+
+    def getId(self) -> int:
+        return self.id
+
+    def getFigure(self) -> str:
+        return self.__class__.__name__
+
+    def toString(self) -> str:
+        raise NotImplementedError("Subclasses should implement this method")
+
+
+class Point(Geometry):
+    def __init__(self, point: Tuple[int, int]):
+        super().__init__()
         self.point = point
 
     def getPoint(self):
         return self.point
 
+    def toString(self) -> str:
+        return f"({self.point[0]},{self.point[1]})"
 
-class Line(Point):
+
+class Line(Geometry):
     def __init__(self, point1: Point, point2: Point):
+        super().__init__()
         self.line = [point1, point2]
 
     def getLine(self):
         return self.line
 
+    def toString(self) -> str:
+        return f"{Point(self.line[0]).toString()}, {Point(self.line[1]).toString()}"
 
-# class Polygon(Line):
-#     def __init__(self, *points: Point):
-#         self.polygon = []
-#         for point in points:
-#             self.polygon.append(point)
 
-#     def getPolygon(self):
-#         return self.polygon
-
-class Polygon:
+class Polygon(Geometry):
     def __init__(self, *points: Point):
+        super().__init__()
         self.polygon = list(points)
 
     def getPolygon(self) -> list[Point]:
         return self.polygon
+
+    def toString(self) -> str:
+        return ", ".join(point.toString() for point in self.polygon)
