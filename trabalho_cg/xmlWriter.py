5c422f10ef1cb7c03e4a5bca1ca82d7f6f17df7f
felkng <felipersf374@gmail.com>
2024-08-11 16:07:54 -0300

    Fix xmlWriter
    Co-authored-by: Marcos Marques <mdmarques70@gmail.com>

--- trabalho_cg/xmlWriter.py
+++ trabalho_cg/xmlWriter.py
@@ -8,24 +8,24 @@ class XmlWriter:
     def write(self, points, lines, polygons) -> None:
         root = ET.Element("dados")
         for point in points:
-            x = str(point.getPoint()[0] - 10)
-            y = str(point.getPoint()[1] - 10)
+            x = str(point.getPoint()[0])
+            y = str(point.getPoint()[1])
             ET.SubElement(root, "ponto", x=x, y=y)
 
         for line in lines:
             currentLine = line.getLine()
             reta = ET.SubElement(root, "reta")
             for point in currentLine:
-                x = str(point[0] - 10)
-                y = str(point[1] - 10)
+                x = str(point[0])
+                y = str(point[1])
                 ET.SubElement(reta, "ponto", x=x, y=y)
 
         for polygon in polygons:
             currentPolygon = polygon.getPolygon()
             poligono = ET.SubElement(root, "poligono")
             for point in currentPolygon:
-                x = str(point[0] - 10)
-                y = str(point[1] - 10)
+                x = str(point.getPoint()[0])
+                y = str(point.getPoint()[1])
                 ET.SubElement(poligono, "ponto", x=x, y=y)
         tree = ET.ElementTree(root)
         ET.indent(tree, space="\t", level=0)
